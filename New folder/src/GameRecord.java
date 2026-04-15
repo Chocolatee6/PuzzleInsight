3f213e95a5ae14dcf0cfb777de2cd40255c29ded
Hoang Quoc Anh <hqanhh7605@gmail.com>
2026-04-15 22:53:39 +0700

    Xoa thu muc thua sau khi pull

--- New folder/src/GameRecord.java
+++
@@ -1,9 +0,0 @@
-public class GameRecord {
-    public int score;
-    public int level;
-
-    public GameRecord(int score, int level) {
-        this.score = score;
-        this.level = level;
-    }
-}
